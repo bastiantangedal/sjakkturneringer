3bbd35214595a966f361eaee7ddc93e41d909a0a
Bastian Tangedal <bastian.Tangedal@gmail.com>
2020-10-04 20:53:03 +0200

    sike

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,50 +1,49 @@
 <template>
   <div class="pt-6">
+    <script
+      type="application/javascript"
+      defer
+      src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"
+    ></script>
+
     <div class="field">
-      <label class="label">Navn:</label>
+      <label class="label">Fullt navn:</label>
       <div class="control">
-        <input class="input" type="text"/>
+        <input class="input" type="text" />
       </div>
     </div>
 
     <div class="field">
-      <label class="label">Brukernavn</label>
+      <label class="label">Brukernavn (brukes når du skal logge inn)</label>
       <div class="control">
-        <input
-          class="input"
-          type="text"
-        />
+        <input class="input" type="text" />
       </div>
     </div>
 
     <div class="field">
       <label class="label">Passord:</label>
       <div class="control">
-        <input
-          class="input"
-          type="password"
-        />
+        <input class="input" type="password" />
       </div>
     </div>
 
-    <div class="field">
-      <label class="label">FIDE-tittel</label>
+    <!-- <div class="field">
       <div class="control">
-        <div class="select">
-          <select>
-            <option></option>
-            <option>CM</option>
-            <option>FM</option>
-            <option>IM</option>
-            <option>GM</option>
-            <option>WCM</option>
-            <option>WFM</option>
-            <option>WIM</option>
-            <option>WGM</option>
-          </select>
-        </div>
+        <label class="checkbox">
+          <input @change="checkFide($event)" type="checkbox" />
+          Jeg har en FIDE-tittel
+        </label>
       </div>
     </div>
+
+    <div v-if="hasFideTitle" class="field">
+      <label class="label">Lenke til FIDE-profil:</label>
+      <div class="control">
+        <input @change="checkUrl" class="input" type="text" />
+      </div>
+    </div> -->
+
+    <span v-if="hasFideTitle" class="tag is-success is-light">Success</span>
 
     <div class="field">
       <div class="control">
@@ -56,15 +55,36 @@
     </div>
 
     <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-dark">Opprett bruker</button>
+      <div class="control pt-3">
+        <button class="button is-success">Opprett bruker</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from '@vue/composition-api';
+
+export default {
+  setup() {
+    const hasFideTitle = ref(false);
+
+    function checkFide($event) {
+      const isChecked = $event.srcElement.checked;
+      hasFideTitle.value = isChecked;
+    }
+
+    function checkUrl($event) {
+      console.log($event.srcElement.className);
+    }
+
+    return {
+      checkFide,
+      hasFideTitle,
+      checkUrl,
+    };
+  },
+};
 </script>
 
 <style>
